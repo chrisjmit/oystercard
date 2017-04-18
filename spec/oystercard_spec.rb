8eb97eacb23c525aa37496e551f37e9bf28461f8
milesillsley <milesillsley@hotmail.co.uk>
2017-04-18 15:25:06 +0100

    Add balance method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,10 @@
 require './lib/oystercard'
 
 describe Oystercard do
-    
-    
+  it { is_expected.to respond_to(:balance)}
+  it "#balance" do
+    expect(subject.balance).to eq(0)
+  end
+
+
 end
