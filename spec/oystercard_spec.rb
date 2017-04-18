e30ee12d664ac24ff8f4f19f5fff48f6a899ef42
SimonTanner <simonthetanner@hotmail.com>
2017-04-18 15:16:13 +0000

    Refactored to initialise with a balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,17 @@ require './lib/oystercard'
 
 describe Oystercard do
   it { is_expected.to respond_to(:balance)}
+  
   it "#balance" do
     expect(subject.balance).to eq(0)
   end
-
+  
+  it "Has a balance of zero when Oystercard is instantiated" do
+    oyster = subject
+    expect(oyster::balance).to eq(0)
+    
+  end
+  
+  it { is_expected.to have_attributes(:balance => 0) }
 
 end
