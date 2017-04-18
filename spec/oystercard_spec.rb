52ca85463e8e21aa5fa18022cb0d8956e7bffc68
milesillsley <milesillsley@hotmail.co.uk>
2017-04-18 16:43:41 +0100

    Implement top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,18 +1,16 @@
 require './lib/oystercard'
 
 describe Oystercard do
+
   it { is_expected.to respond_to(:balance)}
-  
+
   it "#balance" do
     expect(subject.balance).to eq(0)
   end
-  
-  it "Has a balance of zero when Oystercard is instantiated" do
-    oyster = subject
-    expect(oyster::balance).to eq(0)
-    
+
+  it "#top_up" do
+    expect{ subject.top_up(5) }.to change{ subject.balance }.by 5
   end
-  
-  it { is_expected.to have_attributes(:balance => 0) }
+
 
 end
