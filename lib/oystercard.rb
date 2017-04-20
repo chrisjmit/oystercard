77b8f3df792f722b9afd9d36d83cb7e54b411a21
Chris Mitchell <chrisjmit@gmail.com>
2017-04-20 14:55:41 +0100

    Refactored code base, removed respond_to tests, removed additional in_use method, included before blocks

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,43 +1,37 @@
 class Oystercard
 
-  attr_reader :balance, :in_use, :entry_station
-  
+  attr_reader :balance, :entry_station
+
   MaxBalance = 90
   MinBalance = 1
   MinFare = 1
 
   def initialize
     @balance = 0
-    @in_use = false
   end
 
   def top_up(amount)
-    raise "You cannot exceed a balance of #{MaxBalance}" if @balance + amount >= MaxBalance
+    raise "You cannot exceed a balance of #{MaxBalance}" if @balance + amount > MaxBalance
     @balance += amount
   end
 
-  def touch_in(station = 'station')
+  def touch_in(entry_station)
     fail "Your balance is below #{MinBalance} so you cannot travel" if @balance < MinBalance
-  	@in_use = true
-  	@entry_station = station
+    @entry_station = entry_station
+
   end
 
   def touch_out
-  	@in_use = false
   	@entry_station = nil
   	deduct(MinFare)
   end
 
   def in_journey?
-  	if @entry_station == nil 
-  	  return false
-  	else
-  	  return true
-  	end
+    !!entry_station
   end
-  
+
   private
-  
+
   def deduct(amount)
     @balance -= amount
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,9 @@ require './lib/oystercard'
 
 describe Oystercard do
 
-  describe '#balance' do
-    it { is_expected.to respond_to(:balance)}
+  let(:station) { double :station }
 
+  describe '#balance' do
     it "#balance" do
       expect(subject.balance).to eq(0)
     end
@@ -14,39 +14,41 @@ describe Oystercard do
     it "#top_up" do
       expect{ subject.top_up(5) }.to change{ subject.balance }.by 5
     end
-    
+
     it "Raises an error if the balance exceeds 90" do
       expect { subject.top_up(100) }.to raise_error 'You cannot exceed a balance of 90'
     end
   end
 
   describe '#touch_in' do
+
+    before(:example) { subject.top_up(Oystercard::MinFare) }
+
     it 'should let us touch_in a card' do
       is_expected.to respond_to(:touch_in)
     end
 
     it "should return true when we touch_in" do
-      subject.top_up(1)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject).to be_in_journey
-    end
-    
-    it "should raise an error if we try to touch in with a balance of less than 1" do
-      expect { subject.touch_in }.to raise_error 'Your balance is below 1 so you cannot travel'
     end
 
     it "should remember the entry station after we touch in" do
-      station1 = double('station')
-      allow(station1).to receive(:name) {'waterloo'}
-      subject.top_up(1)
-      subject.touch_in(station1.name)
-      expect(subject.entry_station).to eq 'waterloo'
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
     end
-      
+  end
+
+  describe '#touch_in_without_balance' do
+    it "should raise an error if we try to touch in with a balance of less than 1" do
+      expect { subject.touch_in("station") }.to raise_error "Your balance is below 1 so you cannot travel"
+    end
   end
 
   describe '#touch_out' do
-    
+
+    before(:example) { subject.top_up(1) }
+
     it 'should let us touch_out a card' do
       is_expected.to respond_to(:touch_out)
     end
@@ -55,53 +57,36 @@ describe Oystercard do
       subject.touch_out
       expect(subject).not_to be_in_journey
     end
-    
+
     it "should deduct money when a user touches out" do
       expect { subject.touch_out }.to change{ subject.balance}.by (-Oystercard::MinFare)
     end
-    
+
     it "should 'forget' the entry station after we touch out" do
-      station1 = double('station')
-      allow(station1).to receive(:name) {'waterloo'}
-      subject.top_up(1)
-      subject.touch_in(station1.name)
+      subject.touch_in(station)
       subject.touch_out
       expect(subject.entry_station).to eq nil
     end
-    
   end
 
-  describe '#in_use' do
-    it "should return false if we haven't touched_in before" do
-      subject.in_use.should be false
-    end
-  end
 
-  describe '#in_journey?' do 
-  
-    it 'should be a method' do
-      is_expected.to respond_to(:in_journey?)
-    end
-    
+  describe '#in_journey?' do
+
+    before(:example) { subject.top_up(1) }
+
     it "should tell us if we have touched in and touched out" do
-      subject.top_up(1)
-      subject.touch_in
       subject.touch_out
       subject.in_journey?.should be false
     end
-    
-    it "should  return true when entry_station is not set to nil" do
-      station1 = double('station')
-      allow(station1).to receive(:name) {'waterloo'}
-      subject.top_up(1)
-      subject.touch_in(station1)
+
+    it "should return true when entry_station is not set to nil" do
+      subject.touch_in(station)
       subject.in_journey?.should be true
     end
-    
+
     it "should return false when entry_station is set to nil" do
       subject.in_journey?.should be false
     end
-    
   end
 
 end
